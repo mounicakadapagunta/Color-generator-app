4e640175582a695a558fb43186e05a9a9583bfe0
mounicakadaagunta <mounicakadapagunta13@gmail.com>
2022-02-28 13:39:15 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Try this : https://devroopam-color-generator.netlify.app/
+
+
+#### Values JS
+
+[values.js](https://github.com/noeldelgado/values.js)
+# color-generator-react
+
---
+++ src/SingleColor.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import rgbToHex from './utils'
+
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  console.log(hexColor);
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+  // console.log(bcg);
+
+  const hex = rgbToHex(...rgb);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+      setAlert(false);
+    }, 3000)
+    return () => clearTimeout(timeout);
+  }, [alert])
+
+
+  return <article className={`color ${index > 10 && 'color-light'}`}
+    style={{ backgroundColor: `rgb(${bcg})` }}
+    onClick={() => {
+      setAlert(true);
+      navigator.clipboard.writeText(hex);
+    }}>
+    <p className='percent-value'>{weight}%</p>
+    <p className='color-value'>{hex}</p>
+    <br></br>
+    {alert && <p className='alert'>copied to clipboard</p>}
+  </article>
+}
+
+export default SingleColor;
